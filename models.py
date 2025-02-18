282b5e0a95eeb65b8c3a81ff29d34b991d48e134
vernados123 <116774209+vernados123@users.noreply.github.com>
2025-02-18 00:51:17 +0000

    working

--- models.py
+++ models.py
@@ -1,11 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.expression import func
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql.expression import func 
 from werkzeug.security import generate_password_hash
 from app import app
-
 db = SQLAlchemy(app)
-
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -20,27 +17,43 @@ class User(db.Model):
     self.email = email
     self.set_password(password)
 
-  #def create_todo(self, text):
-  #new_todo = Todo(text)
-  #self.todos.append(new_todo)
-  #return new_todo
-  #CODE UNREACHABLE
-
   def set_password(self, password):
       """Create hashed password."""
-      self.password = generate_password_hash(password)
+      self.password = generate_password_hash(password, method='scrypt')
 
   def __repr__(self):
       return f'<User {self.id} {self.username} - {self.email}>'
 
+  # Add method to User model in models.py
+  def add_todo_category(self, todo_id, category_text):
+      # Fetch the todo by id
+      todo = Todo.query.filter_by(id=todo_id, user_id=self.id).first()
+      # Make sure the todo exists and belongs to the current user
+      if not todo:
+          return False
 
+      # Check if category already exists for current user
+      category = Category.query.filter_by(text=category_text, user_id=self.id).first()
+      if not category:
+          # Create new category
+          category = Category(user_id=self.id, text=category_text)
+          db.session.add(category)
+          db.session.commit()
+
+      # Associate todo with the category if not already associated
+      if category not in todo.categories:
+          todo.categories.append(category)
+          db.session.add(todo)
+          db.session.commit()
+
+      return True
 
 class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id 
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+  #set userid as a foreign key to user.id 
   text = db.Column(db.String(255), nullable=False)
   done = db.Column(db.Boolean, default=False)
-  # user = <user object>
 
   def toggle(self):
     self.done = not self.done
@@ -51,6 +64,29 @@ class Todo(db.Model):
       self.text = text
 
   def __repr__(self):
+    category_names = ', '.join([category.text for category in self.categories])
+    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" } | categories [{category_names}]>' 
 
-    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" }>'
+class TodoCategory(db.Model):
+  __tablename__ ='todo_category'
+  id = db.Column(db.Integer, primary_key=True)
+  todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
+  category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+  last_modified = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
 
+  def __repr__(self):
+    return f'<TodoCategory last modified {self.last_modified.strftime("%Y/%m/%d, %H:%M:%S")}>'
+
+class Category(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  text = db.Column(db.String(255), nullable=False)
+  user = db.relationship('User', backref=db.backref('categories', lazy='joined'))
+  todos = db.relationship('Todo', secondary='todo_category', backref=db.backref('categories', lazy=True))
+
+  def __init__(self, user_id, text):
+    self.user_id = user_id
+    self.text = text
+
+  def __repr__(self):
+    return f'<Category user:{self.user.username} - {self.text}>'
--- wsgi.py
+++ wsgi.py
@@ -1,120 +1,138 @@
 import click, sys
 from models import db, User, Todo
-from app import app
 from sqlalchemy.exc import IntegrityError
+from app import app
 
 
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
-  db.drop_all()
-  db.init_app(app)
-  db.create_all()
-  bob = User('bob', 'bob@mail.com', 'bobpass')
-  #new_todo = bob.create_todo("wash car")
-  print(bob)
-  print('database intialized')
-  db.session.add(bob)
-  #db.session.add(new_todo)
-  db.session.commit()
-  print(bob)
+    db.drop_all()
+    db.init_app(app)
+    db.create_all()
+    bob = User('bob', 'bob@mail.com', 'bobpass')
+    bob.todos.append(Todo('wash car'))
+    db.session.add(bob)
+    db.session.commit()
+    print(bob)
+    print('database intialized')
 
-@app.cli.command('delete-user')
-@click.argument('username', default='bob')
-def delete_user(username):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-      print(f'{username} not found!')
-      return
-  db.session.delete(bob)
-  db.session.commit()
-  print(f'{username} deleted')
 
-@app.cli.command("get-user", help="Retrieves a User")
-@click.argument('username', default='bob')
-def get_user(username):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-    print(f'{username} not found!')
-    return
-  print(bob)
+@app.cli.command("get-user", help="Retrieves a User by username or id")
+@click.argument('key', default='bob')
+def get_user(key):
+    bob = User.query.filter_by(username=key).first()
+    if not bob:
+      bob = User.query.get(int(key))
+      if not bob:
+        print(f'{key} not found!')
+        return
+    print(bob)
 
-@app.cli.command('get-users')
-def get_users():
-  # gets all objects of a model
-  users = User.query.all()
-  print(users)
 
 @app.cli.command("change-email")
 @click.argument('username', default='bob')
 @click.argument('email', default='bob@mail.com')
 def change_email(username, email):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
       print(f'{username} not found!')
       return
-  bob.email = email
-  db.session.add(bob)
-  db.session.commit()
-  print(bob)
+    bob.email = email
+    db.session.add(bob)
+    db.session.commit()
+    print(bob)
+
+
+@app.cli.command('get-users')
+def get_users():
+    users = User.query.all()
+    print(users)
+
 
 @app.cli.command('create-user')
 @click.argument('username', default='rick')
 @click.argument('email', default='rick@mail.com')
 @click.argument('password', default='rickpass')
 def create_user(username, email, password):
-  newuser = User(username, email, password)
-  try:
-    db.session.add(newuser)
-    db.session.commit()
-  except IntegrityError as e:
-    #let's the database undo any previous steps of a transaction
-    db.session.rollback()
-    # print(e.orig) #optionally print the error raised by the database
-    print("Username or email already taken!") #give the user a useful message
-  else:
-    print(newuser) # print the newly created user
+    newuser = User(username, email, password)
+    try:
+      db.session.add(newuser)
+      db.session.commit()
+    except IntegrityError as e:
+      db.session.rollback()
+      print(e.orig)
+      print("Username or email already taken!")
+    else:
+      print(newuser)
 
-#just putting this here for the lab but i cant firgure out why my code in models.py is unreachable after the new todos command
 
-@app.cli.command('get-todos')
+@app.cli.command('delete-user')
 @click.argument('username', default='bob')
-def get_user_todos(username):
-  bob = User.query.filter_by(username=username).first()
-  if not bob:
-      print(f'{username} not found!')
-      return
-  print(bob.todos)
-
-  @app.cli.command('add-todo')
-  @click.argument('username', default='bob')
-  @click.argument('text', default='Mow lawn')
-  def add_task(username, text):
+def delete_user(username):
     bob = User.query.filter_by(username=username).first()
     if not bob:
-        print(f'{username} not found!')
-        return
+      print(f'{username} not found!')
+      return
+    db.session.delete(bob)
+    db.session.commit()
+    print(f'{username} deleted')
+
+
+@app.cli.command('add-todo')
+@click.argument('username', default='bob')
+@click.argument('text', default='wash car')
+def add_task(username, text):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+      print(f'{username} not found!')
+      return
     new_todo = Todo(text)
     bob.todos.append(new_todo)
     db.session.add(bob)
     db.session.commit()
+    print('Todo added!')
+
 
 @app.cli.command('get-todos')
-def get_todos():
-  todos = Todo.query.all()
-  print(todos)
+@click.argument('username', default='bob')
+def get_user_todos(username):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+      print(f'{username} not found!')
+      return
+    print(bob.todos)
+
 
 @click.argument('todo_id', default=1)
 @click.argument('username', default='bob')
 @app.cli.command('toggle-todo')
 def toggle_todo_command(todo_id, username):
-  user = User.query.filter_by(username=username).first()
-  if not user:
-    print(f'{username} not found!')
-    return
+    user = User.query.filter_by(username=username).first()
+    if not user:
+      print(f'{username} not found!')
+      return
 
-  todo = Todo.query.filter_by(id=todo_id, user_id=user.id).first()
-  if not todo:
-    print(f'{username} has no todo id {todo_id}')
+    todo = Todo.query.filter_by(id=todo_id, user_id=user.id).first()
+    if not todo:
+      print(f'{username} has no todo id {todo_id}')
 
-  todo.toggle()
-  print(f'{todo.text} is {"done" if todo.done else "not done"}!')
+    todo.toggle()
+    print(f'{todo.text} is {"done" if todo.done else "not done"}!')
+
+@click.argument('category', default='chores')
+@click.argument('username', default='bob')
+@click.argument('todo_id', default=1)
+@app.cli.command('add-category', help="Adds a category to a todo")
+def add_todo_category_command(category, todo_id, username,):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+      print(f'user {username} not found!')
+      return
+
+    res = user.add_todo_category(todo_id, category)
+    if not res:
+      print(f'{username} has no todo id {todo_id}')
+      return
+
+    todo = Todo.query.get(todo_id)
+    print(todo)
