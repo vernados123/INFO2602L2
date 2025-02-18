b115b35bb797c83cbaed5911bfddbbe6ac1c4c7e
vernados123 <116774209+vernados123@users.noreply.github.com>
2025-02-18 00:33:21 +0000

    new todo code unreachable

--- models.py
+++ models.py
@@ -12,15 +12,45 @@ class User(db.Model):
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(120), nullable=False)
+  #creates a relationship field to get the user's todos
+  todos = db.relationship('Todo', backref='user', lazy=True, cascade="all, delete-orphan")
 
   def __init__(self, username, email, password):
     self.username = username
     self.email = email
     self.set_password(password)
 
+  #def create_todo(self, text):
+  #new_todo = Todo(text)
+  #self.todos.append(new_todo)
+  #return new_todo
+  #CODE UNREACHABLE
+
   def set_password(self, password):
-    """Create hashed password."""
-    self.password = generate_password_hash(password, method='scrypt')
+      """Create hashed password."""
+      self.password = generate_password_hash(password)
 
   def __repr__(self):
-    return f'<User {self.id} {self.username} - {self.email}>'
+      return f'<User {self.id} {self.username} - {self.email}>'
+
+
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id 
+  text = db.Column(db.String(255), nullable=False)
+  done = db.Column(db.Boolean, default=False)
+  # user = <user object>
+
+  def toggle(self):
+    self.done = not self.done
+    db.session.add(self)
+    db.session.commit()
+
+  def __init__(self, text):
+      self.text = text
+
+  def __repr__(self):
+
+    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" }>'
+
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import click, sys
-from models import db, User
+from models import db, User, Todo
 from app import app
 from sqlalchemy.exc import IntegrityError
 
@@ -10,9 +10,11 @@ def initialize():
   db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
+  #new_todo = bob.create_todo("wash car")
   print(bob)
   print('database intialized')
   db.session.add(bob)
+  #db.session.add(new_todo)
   db.session.commit()
   print(bob)
 
@@ -55,25 +57,64 @@ def change_email(username, email):
   db.session.commit()
   print(bob)
 
-@app.cli.command("create-user", help="Creates a new user")
+@app.cli.command('create-user')
 @click.argument('username', default='rick')
 @click.argument('email', default='rick@mail.com')
 @click.argument('password', default='rickpass')
 def create_user(username, email, password):
   newuser = User(username, email, password)
-  msg = ""
-  res
   try:
     db.session.add(newuser)
     db.session.commit()
-  except IntegrityError:
+  except IntegrityError as e:
     #let's the database undo any previous steps of a transaction
     db.session.rollback()
     # print(e.orig) #optionally print the error raised by the database
-    msg = ("Username or email already taken!") #give the user a useful message
-    res = None
+    print("Username or email already taken!") #give the user a useful message
   else:
-    msg = str(newuser)# print the newly created user
-finally:
-  print(msg)
-  return res
+    print(newuser) # print the newly created user
+
+#just putting this here for the lab but i cant firgure out why my code in models.py is unreachable after the new todos command
+
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+      print(f'{username} not found!')
+      return
+  print(bob.todos)
+
+  @app.cli.command('add-todo')
+  @click.argument('username', default='bob')
+  @click.argument('text', default='Mow lawn')
+  def add_task(username, text):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+        print(f'{username} not found!')
+        return
+    new_todo = Todo(text)
+    bob.todos.append(new_todo)
+    db.session.add(bob)
+    db.session.commit()
+
+@app.cli.command('get-todos')
+def get_todos():
+  todos = Todo.query.all()
+  print(todos)
+
+@click.argument('todo_id', default=1)
+@click.argument('username', default='bob')
+@app.cli.command('toggle-todo')
+def toggle_todo_command(todo_id, username):
+  user = User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found!')
+    return
+
+  todo = Todo.query.filter_by(id=todo_id, user_id=user.id).first()
+  if not todo:
+    print(f'{username} has no todo id {todo_id}')
+
+  todo.toggle()
+  print(f'{todo.text} is {"done" if todo.done else "not done"}!')
